dcb193e40cda59f57b42160c7b190152eecd27b3
Shabhai Zafar <zafarshabhai03@gmail.com>
2024-09-08 13:41:54 +0530

    Operators1

---
+++ Operators/Operators.js
@@ -0,0 +1,57 @@
+// Arithatic Operators : 
+    // +,-,*,/,%
+
+// Assignment Operators : 
+    // =,+=,-=,*=,/=,%=
+
+// Relational Operators//Comparation Operators : 
+    // <,>,<=,>=,==,!=, New(===,!==)
+
+// Type of Operators :
+    // typeof
+
+// Logical Operators  : 
+    // AND (&&),OR (||),NOT (!)
+
+// Ternary Operator(Contional Operator)
+    // (condition)?statement1 : statement2;
+
+
+//Arithatic Operators : 
+// var n1 = 12;
+// var n2 = 13;
+// console.log("Value of N1 : ",n1);
+// console.log("Value of N2 : ",n2);
+
+// console.log("Addition is : ",n1+n2);
+// // n1,n2 are Operands and + is Operator
+
+// console.log("Subtraction is : ",n1-n2);
+// // n1,n2 are Operands and - is Operator
+
+// console.log("Multiplication is : ",n1*n2);
+// // n1,n2 are Operands and * is Operator
+
+// console.log("Division is : ",n1/n2);
+// // n1,n2 are Operands and / is Operator
+
+// console.log("Modulo is : ",n1%n2);
+// // n1,n2 are Operands and % is Operator
+
+
+
+
+
+
+
+//prompt : user input
+
+// scanf  : user input
+
+var n1 = +prompt("Enter the Value of N1 :");
+var n2 = +prompt("Enter the Value of N2 :");
+console.log("Value of N1 : ",n1);
+console.log("Value of N2 : ",n2);
+
+
+console.log("Addition is : ",n1+n2);
